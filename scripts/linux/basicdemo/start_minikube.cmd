bc0dfb074f64732d3130c467ad3a1d1344e86cdb
Miguel Torres <migueltorreslopez@gmail.com>
2021-10-29 20:57:59 -0700

    Add windows Installer

--- scripts/linux/basicdemo/start_minikube.cmd
+++ scripts/linux/basicdemo/start_minikube.cmd
@@ -1,4 +1,5 @@
 @echo off
-set minikube_ip=$(minikube ip)
-export AIR_MQTT_HOSTNAME=%minikube_ip%
+for /f "delims=" %%i in ('minikube ip') do cd "%%i"
+FOR /F "tokens=1 delims= " %%G IN ('minikube ip') DO (set minikube_ip=%%G)
+set AIR_MQTT_HOSTNAME=%minikube_ip%
 call start.cmd
