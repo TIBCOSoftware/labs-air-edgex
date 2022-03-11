1eca63223815181a5e028b2964987fd92c81628f
Miguel Torres <migueltorreslopez@gmail.com>
2022-03-11 08:57:18 -0800

    change start.sh script to start all docker-compose services

--- scripts/linux/basicdemo/start.cmd
+++ scripts/linux/basicdemo/start.cmd
@@ -2,63 +2,8 @@
 set COMPOSE_FILE=docker-compose.yml
 echo "Using compose file: %COMPOSE_FILE%"
 
-
-
-set EDGEX_TOKEN=""
-
 echo  " Starting.. %COMPOSE_FILE%"
-docker-compose -f "%COMPOSE_FILE%" up -d security-secrets-setup
-
-docker-compose -f "%COMPOSE_FILE%" up -d consul
-
-docker-compose -f "%COMPOSE_FILE%" up -d database
-
-docker-compose -f "%COMPOSE_FILE%" up -d notifications
-
-docker-compose -f "%COMPOSE_FILE%" up -d metadata
-
-docker-compose -f "%COMPOSE_FILE%" up -d vault-worker
-
-docker-compose -f "%COMPOSE_FILE%" up -d security-bootstrap-database
-
-docker-compose -f "%COMPOSE_FILE%" up -d mqtt-broker
-
-docker-compose -f "%COMPOSE_FILE%" up -d vault
-
-docker-compose -f "%COMPOSE_FILE%" up -d kong-db
-
-docker-compose -f "%COMPOSE_FILE%" up -d kong-migration
-timeout 2
-
-docker-compose -f "%COMPOSE_FILE%" up -d kong
-timeout 5
-
-docker-compose -f "%COMPOSE_FILE%" up -d edgex-proxy
-
-docker-compose -f "%COMPOSE_FILE%" up -d scheduler
-
-docker-compose -f "%COMPOSE_FILE%" up -d system
-
-docker-compose -f "%COMPOSE_FILE%" up -d data
-
-docker-compose -f "%COMPOSE_FILE%" up -d command
-
-
-echo "Replacing token!!"
-docker-compose run --rm --entrypoint /edgex/security-proxy-setup edgex-proxy --init=false --useradd=tibuser --group=admin > secToken.txt
-findstr /B /C:"the access token for user tibuser is" secToken.txt > setTokenLine.txt
-FOR /F "tokens=8 delims= " %%G IN (setTokenLine.txt) DO (set token=%%G)
-set EDGEX_TOKEN=%token:~0,-1%
-
-
-docker-compose -f "%COMPOSE_FILE%" up -d service-metadata
-timeout 5
-
-::KEEP DISSABLED: run_service device-siemens-simulator
-
-docker-compose -f "%COMPOSE_FILE%" up -d device-generic-mqtt
-
-docker-compose -f "%COMPOSE_FILE%" up -d device-generic-rest
+docker-compose -f "%COMPOSE_FILE%" up
 
 echo "\n"
 echo  "Installing cors plugin"
