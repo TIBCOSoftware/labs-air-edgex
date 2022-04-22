1c263c20ee73f5bfe000f612b16dedbd422c664b
Miguel Torres <migueltorreslopez@gmail.com>
2022-04-22 08:44:47 -0700

    Add error exit code

--- scripts/linux/basicdemo/export.sh
+++ scripts/linux/basicdemo/export.sh
@@ -3,28 +3,28 @@ mkdir -p ./archives
 
 source .env
 
-docker-compose pull
+docker-compose pull || exit 1
 
-docker save --output ./archives/core-command:${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/core-command:${EDGEX_FOUNDRY_VERSION} || exit 2
+docker save --output ./archives/core-command:${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/core-command:${EDGEX_FOUNDRY_VERSION} || exit 1
 
-docker save --output ./archives/consul-${CONSUL_VERSION}.tar consul:${CONSUL_VERSION}
+docker save --output ./archives/consul-${CONSUL_VERSION}.tar consul:${CONSUL_VERSION} || exit 1
 
-docker save --output ./archives/core-data-${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/core-data:${EDGEX_FOUNDRY_VERSION}
+docker save --output ./archives/core-data-${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/core-data:${EDGEX_FOUNDRY_VERSION} || exit 1
 
-docker save --output ./archives/redis-${REDIS_VERSION}.tar redis:${REDIS_VERSION}
+docker save --output ./archives/redis-${REDIS_VERSION}.tar redis:${REDIS_VERSION} || exit 1
 
-docker save --output ./archives/core-metadata-${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/core-metadata:${EDGEX_FOUNDRY_VERSION}
+docker save --output ./archives/core-metadata-${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/core-metadata:${EDGEX_FOUNDRY_VERSION} || exit 1
 
-docker save --output ./archives/eclipse-mosquitto-${ECLIPSE_MOSQUITTO_VERSION}.tar eclipse-mosquitto:${ECLIPSE_MOSQUITTO_VERSION}
+docker save --output ./archives/eclipse-mosquitto-${ECLIPSE_MOSQUITTO_VERSION}.tar eclipse-mosquitto:${ECLIPSE_MOSQUITTO_VERSION} || exit 1
 
-docker save --output ./archives/support-notifications-${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/support-notifications:${EDGEX_FOUNDRY_VERSION}
+docker save --output ./archives/support-notifications-${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/support-notifications:${EDGEX_FOUNDRY_VERSION} || exit 1
 
-docker save --output ./archives/support-scheduler-${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/support-scheduler:${EDGEX_FOUNDRY_VERSION}
+docker save --output ./archives/support-scheduler-${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/support-scheduler:${EDGEX_FOUNDRY_VERSION} || exit 1
 
-docker save --output ./archives/sys-mgmt-agent-${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/sys-mgmt-agent:${EDGEX_FOUNDRY_VERSION}
+docker save --output ./archives/sys-mgmt-agent-${EDGEX_FOUNDRY_VERSION}.tar edgexfoundry/sys-mgmt-agent:${EDGEX_FOUNDRY_VERSION} || exit 1
 
-docker save --output ./archives/labs-air-edgex-app-service-metadata-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-air-edgex-app-service-metadata:${LABS_AIR_VERSION}
+docker save --output ./archives/labs-air-edgex-app-service-metadata-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-air-edgex-app-service-metadata:${LABS_AIR_VERSION} || exit 1
 
-docker save --output ./archives/labs-air-edgex-device-generic-rest-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-air-edgex-device-generic-rest:${LABS_AIR_VERSION}
+docker save --output ./archives/labs-air-edgex-device-generic-rest-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-air-edgex-device-generic-rest:${LABS_AIR_VERSION} || exit 1
 
-docker save --output ./archives/labs-air-edgex-device-generic-mqtt-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-air-edgex-device-generic-mqtt:${LABS_AIR_VERSION}
+docker save --output ./archives/labs-air-edgex-device-generic-mqtt-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-air-edgex-device-generic-mqtt:${LABS_AIR_VERSION} || exit 1
