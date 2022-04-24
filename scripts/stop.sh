4865d91c798b699ee8d32cf98259d741f92f3e4a
Miguel Torres <migueltorreslopez@gmail.com>
2022-04-23 17:07:18 -0700

    Add arch different installer logic

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -6,10 +6,10 @@ arch_type=${3:?}
 
 if [[ "${arch_type}" == "amd64" ]]; then
     pushd ./linux/basicdemo > /dev/null || exit 1
-    ./stop.sh || exit 2
+    ./stop.sh
     popd || exit 1
 elif [[ "${arch_type}" == "arm64" ]]; then
     pushd ./arm64/basicdemo > /dev/null || exit 1
-    ./stop.sh || exit 2
+    ./stop.sh
     popd || exit 1
 fi
