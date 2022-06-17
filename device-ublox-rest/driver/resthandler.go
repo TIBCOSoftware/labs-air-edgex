dfd108673c8bd18a780f607c014793992d88f5c8
magallardo <gallardo@tibco.com>
2022-06-17 16:53:06 -0400

    Initial revision

---
+++ device-ublox-rest/driver/resthandler.go
@@ -0,0 +1,486 @@
+//
+// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2021 IOTech Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package driver
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"time"
+	"strings"
+
+	"github.com/edgexfoundry/device-sdk-go/v2/pkg/models"
+	sdk "github.com/edgexfoundry/device-sdk-go/v2/pkg/service"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/clients/logger"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/common"
+	model "github.com/edgexfoundry/go-mod-core-contracts/v2/models"
+	"github.com/gorilla/mux"
+	"github.com/spf13/cast"
+)
+
+const (
+	apiDevicePath  = common.ApiBase + "/device/"
+	apiDeviceRoute  = common.ApiBase + "/device/{" + common.DeviceName + "}"
+	apiResourceRoute  = common.ApiBase + "/resource/{" + common.DeviceName + "}/{" + common.ResourceName + "}"
+	handlerContextKey = "RestHandler"
+)
+
+type UbloxEvent struct {
+	PowerOn bool `json:"poweron"`
+}
+type UbloxSensor struct {
+	Temp float32 `json:"temp"`
+}
+type UbloxGnss struct {
+	Lat float32 `json:"lat"`
+	Lon float32 `json:"lon"`
+	Qty int16 `json:"qty"`
+}
+type UbloxLocation struct {
+	Gnss UbloxGnss `json:"gnss"`
+}
+type UBloxMsg struct {
+	Event UbloxEvent
+	Sensor UbloxSensor
+	Loc UbloxLocation
+}
+
+type RestHandler struct {
+	service     *sdk.DeviceService
+	logger      logger.LoggingClient
+	asyncValues chan<- *models.AsyncValues
+}
+
+func NewRestHandler(service *sdk.DeviceService, logger logger.LoggingClient, asyncValues chan<- *models.AsyncValues) *RestHandler {
+	handler := RestHandler{
+		service:     service,
+		logger:      logger,
+		asyncValues: asyncValues,
+	}
+
+	return &handler
+}
+
+func (handler RestHandler) Start() error {
+	if err := handler.service.AddRoute(apiResourceRoute, handler.addContext(deviceHandler), http.MethodPost); err != nil {
+		return fmt.Errorf("unable to add required route: %s: %s", apiResourceRoute, err.Error())
+	}
+
+	handler.logger.Infof("Route %s added.", apiResourceRoute)
+
+	if err := handler.service.AddRoute(apiDeviceRoute, handler.addContext(deviceHandler), http.MethodPost); err != nil {
+		return fmt.Errorf("unable to add required route: %s: %s", apiDeviceRoute, err.Error())
+	}
+
+	handler.logger.Infof("Route %s added.", apiDeviceRoute)
+
+	return nil
+}
+
+func (handler RestHandler) addContext(next func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	// Add the context with the handler so the endpoint handling code can get back to this handler
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), handlerContextKey, handler) //nolint
+		next(w, r.WithContext(ctx))
+	})
+}
+
+func (handler RestHandler) processAsyncRequest(writer http.ResponseWriter, request *http.Request) {
+
+	handler.logger.Debugf("Received POST request URI%s", request.RequestURI)
+
+	if strings.HasPrefix(string(request.RequestURI), apiDevicePath) {
+		handler.processDeviceRequest(writer, request)
+	} else {
+		handler.processResourceRequest(writer, request)
+	}
+}
+
+func (handler RestHandler) processDeviceRequest(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	deviceName := vars[common.DeviceName]
+
+	handler.logger.Debugf("Received POST for Device=%s", deviceName)
+
+	_, err := handler.service.GetDeviceByName(deviceName)
+	if err != nil {
+		handler.logger.Errorf("Incoming reading ignored. Device '%s' not found", deviceName)
+		http.Error(writer, fmt.Sprintf("Device '%s' not found", deviceName), http.StatusNotFound)
+		return
+	}
+
+	contentType := request.Header.Get(common.ContentType)
+
+	handler.logger.Debugf("Received contentType=%s", contentType)
+
+	// var reading map[string]interface{}
+	var reading UBloxMsg
+
+	body, err := handler.readBody(request)
+	if err != nil {
+		handler.logger.Errorf("Incoming reading ignored. Unable to read request body: %s", err.Error())
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+	handler.logger.Debugf("Received body=%v", body)
+
+	json.Unmarshal(body, &reading)
+
+	handler.logger.Debugf("Received reading=%v", reading)
+
+	handler.logger.Debugf("Received reading Lat=%d", reading.Loc.Gnss.Lat)
+
+	// Handle temperature resource reading
+	resourceName := "temperature"
+	deviceResource, ok := handler.service.DeviceResource(deviceName, resourceName)
+	if !ok {
+		handler.logger.Errorf("Incoming reading ignored. Resource '%s' not found", resourceName)
+		http.Error(writer, fmt.Sprintf("Resource '%s' not found", resourceName), http.StatusNotFound)
+		return
+	}
+
+	value, err := handler.newCommandValue(deviceResource, reading.Sensor.Temp, deviceResource.Properties.ValueType, contentType)
+	if err != nil {
+		handler.logger.Errorf("Incoming reading ignored. Unable to create Command Value for Device=%s Command=%s: %s",
+			deviceName, resourceName, err.Error())
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	asyncValues := &models.AsyncValues{
+		DeviceName:    deviceName,
+		CommandValues: []*models.CommandValue{value},
+	}
+
+	handler.asyncValues <- asyncValues
+
+	// Handle location resource reading
+	resourceName = "location"
+	deviceResource, ok = handler.service.DeviceResource(deviceName, resourceName)
+	if !ok {
+		handler.logger.Errorf("Incoming reading ignored. Resource '%s' not found", resourceName)
+		http.Error(writer, fmt.Sprintf("Resource '%s' not found", resourceName), http.StatusNotFound)
+		return
+	}
+
+	locationReading := fmt.Sprintf("%f|%f|%d", reading.Loc.Gnss.Lat, reading.Loc.Gnss.Lon, reading.Loc.Gnss.Qty)
+	value, err = handler.newCommandValue(deviceResource, locationReading, deviceResource.Properties.ValueType, contentType)
+	if err != nil {
+		handler.logger.Errorf("Incoming reading ignored. Unable to create Command Value for Device=%s Command=%s: %s",
+			deviceName, resourceName, err.Error())
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	asyncValues = &models.AsyncValues{
+		DeviceName:    deviceName,
+		CommandValues: []*models.CommandValue{value},
+	}
+
+	handler.asyncValues <- asyncValues
+}
+
+func (handler RestHandler) processResourceRequest(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	deviceName := vars[common.DeviceName]
+	resourceName := vars[common.ResourceName]
+
+	handler.logger.Debugf("Received POST for Device=%s Resource=%s", deviceName, resourceName)
+
+	_, err := handler.service.GetDeviceByName(deviceName)
+	if err != nil {
+		handler.logger.Errorf("Incoming reading ignored. Device '%s' not found", deviceName)
+		http.Error(writer, fmt.Sprintf("Device '%s' not found", deviceName), http.StatusNotFound)
+		return
+	}
+
+	deviceResource, ok := handler.service.DeviceResource(deviceName, resourceName)
+	if !ok {
+		handler.logger.Errorf("Incoming reading ignored. Resource '%s' not found", resourceName)
+		http.Error(writer, fmt.Sprintf("Resource '%s' not found", resourceName), http.StatusNotFound)
+		return
+	}
+
+	contentType := request.Header.Get(common.ContentType)
+
+	var reading interface{}
+
+	data, err := handler.readBody(request)
+	if err != nil {
+		handler.logger.Errorf("Incoming reading ignored. Unable to read request body: %s", err.Error())
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if deviceResource.Properties.ValueType == common.ValueTypeBinary || deviceResource.Properties.ValueType == common.ValueTypeObject {
+		reading = data
+	} else {
+		reading = string(data)
+	}
+
+	value, err := handler.newCommandValue(deviceResource, reading, deviceResource.Properties.ValueType, contentType)
+	if err != nil {
+		handler.logger.Errorf("Incoming reading ignored. Unable to create Command Value for Device=%s Command=%s: %s",
+			deviceName, resourceName, err.Error())
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	asyncValues := &models.AsyncValues{
+		DeviceName:    deviceName,
+		CommandValues: []*models.CommandValue{value},
+	}
+
+	handler.logger.Debugf("Incoming reading received: Device=%s Resource=%s", deviceName, resourceName)
+
+	handler.asyncValues <- asyncValues
+}
+
+func (handler RestHandler) readBody(request *http.Request) ([]byte, error) {
+	defer request.Body.Close()
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(body) == 0 {
+		return nil, fmt.Errorf("no request body provided")
+	}
+
+	return body, nil
+}
+
+func deviceHandler(writer http.ResponseWriter, request *http.Request) {
+	handler, ok := request.Context().Value(handlerContextKey).(RestHandler)
+	if !ok {
+		writer.WriteHeader(http.StatusBadRequest)
+		_, err := writer.Write([]byte("Bad context pass to handler"))
+		if err != nil {
+			handler.logger.Debugf("problem in writer of byte array: '%s'", err.Error())
+		}
+		return
+	}
+
+	handler.processAsyncRequest(writer, request)
+}
+
+func (handler RestHandler) newCommandValue(resource model.DeviceResource, reading interface{}, valueType string, contentType string) (*models.CommandValue, error) {
+	var err error
+	var result = &models.CommandValue{}
+	castError := "failed to parse %v reading, %v"
+
+	var val interface{}
+	switch valueType {
+	case common.ValueTypeBinary:
+		var ok bool
+		val, ok = reading.([]byte)
+		if !ok {
+			return nil, fmt.Errorf(castError, resource.Name, "not []byte")
+		}
+		if contentType != resource.Properties.MediaType {
+			return nil, fmt.Errorf("wrong Content-Type: expected '%s' but received '%s'", resource.Properties.MediaType, contentType)
+		}
+	case common.ValueTypeObject:
+		if contentType != common.ContentTypeJSON {
+			return nil, fmt.Errorf("wrong Content-Type: expected '%s' but received '%s'", common.ContentTypeJSON, contentType)
+		}
+
+		data, ok := reading.([]byte)
+		if !ok {
+			return nil, fmt.Errorf(castError, resource.Name, "not []byte")
+		}
+
+		val = map[string]interface{}{}
+		if err := json.Unmarshal(data, &val); err != nil {
+			return nil, errors.New("unable to marshal JSON data to type Object")
+		}
+	case common.ValueTypeBool:
+		val, err = cast.ToBoolE(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeString:
+		val, err = cast.ToStringE(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeUint8:
+		val, err = cast.ToUint8E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+		if err := checkUintValueRange(valueType, val); err != nil {
+			return nil, err
+		}
+	case common.ValueTypeUint16:
+		val, err = cast.ToUint16E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+		if err := checkUintValueRange(valueType, val); err != nil {
+			return nil, err
+		}
+	case common.ValueTypeUint32:
+		val, err = cast.ToUint32E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+		if err := checkUintValueRange(valueType, val); err != nil {
+			return nil, err
+		}
+	case common.ValueTypeUint64:
+		val, err = cast.ToUint64E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+		if err := checkUintValueRange(valueType, val); err != nil {
+			return nil, err
+		}
+	case common.ValueTypeInt8:
+		val, err = cast.ToInt8E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+		if err := checkIntValueRange(valueType, val); err != nil {
+			return nil, err
+		}
+	case common.ValueTypeInt16:
+		val, err = cast.ToInt16E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+		if err := checkIntValueRange(valueType, val); err != nil {
+			return nil, err
+		}
+	case common.ValueTypeInt32:
+		val, err = cast.ToInt32E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+		if err := checkIntValueRange(valueType, val); err != nil {
+			return nil, err
+		}
+	case common.ValueTypeInt64:
+		val, err = cast.ToInt64E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+		if err := checkIntValueRange(valueType, val); err != nil {
+			return nil, err
+		}
+	case common.ValueTypeFloat32:
+		val, err = cast.ToFloat32E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+		if err := checkFloatValueRange(valueType, val); err != nil {
+			return nil, err
+		}
+	case common.ValueTypeFloat64:
+		val, err = cast.ToFloat64E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+		if err := checkFloatValueRange(valueType, val); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("return result fail, unsupported value type: %v", valueType)
+	}
+
+	result, err = models.NewCommandValue(resource.Name, valueType, val)
+	if err != nil {
+		return nil, err
+	}
+
+	result.Origin = time.Now().UnixNano() / int64(time.Millisecond)
+	return result, nil
+}
+
+func checkUintValueRange(valueType string, val interface{}) error {
+	switch valueType {
+	case common.ValueTypeUint8:
+		_, ok := val.(uint8)
+		if ok {
+			return nil
+		}
+	case common.ValueTypeUint16:
+		_, ok := val.(uint16)
+		if ok {
+			return nil
+		}
+	case common.ValueTypeUint32:
+		_, ok := val.(uint32)
+		if ok {
+			return nil
+		}
+	case common.ValueTypeUint64:
+		_, ok := val.(uint64)
+		if ok {
+			return nil
+		}
+	}
+	return fmt.Errorf("value %v for %s type is out of range", val, valueType)
+}
+
+func checkIntValueRange(valueType string, val interface{}) error {
+	switch valueType {
+	case common.ValueTypeInt8:
+		_, ok := val.(int8)
+		if ok {
+			return nil
+		}
+	case common.ValueTypeInt16:
+		_, ok := val.(int16)
+		if ok {
+			return nil
+		}
+	case common.ValueTypeInt32:
+		_, ok := val.(int32)
+		if ok {
+			return nil
+		}
+	case common.ValueTypeInt64:
+		_, ok := val.(int64)
+		if ok {
+			return nil
+		}
+	}
+	return fmt.Errorf("value %v for %s type is out of range", val, valueType)
+}
+
+func checkFloatValueRange(valueType string, val interface{}) error {
+	switch valueType {
+	case common.ValueTypeFloat32:
+		valFloat := val.(float32)
+		if math.Abs(float64(valFloat)) >= math.SmallestNonzeroFloat32 && math.Abs(float64(valFloat)) <= math.MaxFloat32 {
+			return nil
+		}
+	case common.ValueTypeFloat64:
+		valFloat := val.(float64)
+		if math.Abs(valFloat) >= math.SmallestNonzeroFloat64 && math.Abs(valFloat) <= math.MaxFloat64 {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("value %v for %s type is out of range", val, valueType)
+}
---
+++ device-ublox-rest/driver/resthandler_test.go
@@ -0,0 +1,130 @@
+//
+// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2021 IOTech Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package driver
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/edgexfoundry/device-sdk-go/v2/pkg/models"
+	sdk "github.com/edgexfoundry/device-sdk-go/v2/pkg/service"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/clients/logger"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/common"
+	model "github.com/edgexfoundry/go-mod-core-contracts/v2/models"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var handler *RestHandler
+
+func TestMain(m *testing.M) {
+	service := &sdk.DeviceService{}
+	logger := logger.NewMockClient()
+	asyncValues := make(chan<- *models.AsyncValues)
+
+	handler = NewRestHandler(service, logger, asyncValues)
+	os.Exit(m.Run())
+}
+
+func TestNewCommandValue(t *testing.T) {
+
+	objectJson := `{
+  "apiVersion": "v2",
+  "path" : "mqtt2",
+  "secretData" : [
+    {
+      "key" : "username",
+      "value" : "app-user"
+    },
+    {
+      "key" : "password",
+      "value" : "SuperDuperSecretPassword"
+    }
+  ]
+}
+`
+	expectedObject := map[string]interface{}{}
+	err := json.Unmarshal([]byte(objectJson), &expectedObject)
+	require.NoError(t, err)
+
+	tests := []struct {
+		Name          string
+		Value         interface{}
+		Expected      interface{}
+		Type          string
+		MimeType      string
+		ContentType   string
+		ErrorExpected bool
+	}{
+		{"Test A Binary", []byte{1, 0, 0, 1}, []byte{1, 0, 0, 1}, common.ValueTypeBinary, "image/jpeg", "image/jpeg", false},
+		{"Test A Binary error", []byte{1, 0, 0, 1}, []byte{1, 0, 0, 1}, common.ValueTypeBinary, "image/jpeg", "image/png", true},
+		{"Test A String JSON", `{"name" : "My JSON"}`, `{"name" : "My JSON"}`, common.ValueTypeString, "", "", false},
+		{"Test A String Text", "Random Text", "Random Text", common.ValueTypeString, "", "text/plain", false},
+		{"Test A Bool true", "true", true, common.ValueTypeBool, "", "text/plain", false},
+		{"Test A Bool false", "false", false, common.ValueTypeBool, "", "text/plain", false},
+		{"Test A Bool error", "bad", nil, common.ValueTypeBool, "", "text/plain", true},
+		{"Test A Float32+", "123.456", float32(123.456), common.ValueTypeFloat32, "", "text/plain", false},
+		{"Test A Float32-", "-123.456", float32(-123.456), common.ValueTypeFloat32, "", "text/plain", false},
+		{"Test A Float32 error", "-123.junk", nil, common.ValueTypeFloat32, "", "text/plain", true},
+		{"Test A Float64+", "456.123", 456.123, common.ValueTypeFloat64, "", "text/plain", false},
+		{"Test A Float64-", "-456.123", -456.123, common.ValueTypeFloat64, "", "text/plain", false},
+		{"Test A Float64 error", "Random", nil, common.ValueTypeFloat64, "", "text/plain", true},
+		{"Test A Uint8", "255", uint8(255), common.ValueTypeUint8, "", "text/plain", false},
+		{"Test A Uint8 error", "FF", nil, common.ValueTypeUint8, "", "text/plain", true},
+		{"Test A Uint16", "65535", uint16(65535), common.ValueTypeUint16, "", "text/plain", false},
+		{"Test A Uint16 error", "FFFF", nil, common.ValueTypeUint16, "", "text/plain", true},
+		{"Test A Uint32", "4294967295", uint32(4294967295), common.ValueTypeUint32, "", "text/plain", false},
+		{"Test A Uint32 error", "FFFFFFFF", nil, common.ValueTypeUint32, "", "text/plain", true},
+		{"Test A Uint64", "6744073709551615", uint64(6744073709551615), common.ValueTypeUint64, "", "text/plain", false},
+		{"Test A Uint64 error", "FFFFFFFFFFFFFFFF", nil, common.ValueTypeUint64, "", "text/plain", true},
+		{"Test A Int8+", "101", int8(101), common.ValueTypeInt8, "", "text/plain", false},
+		{"Test A Int8-", "-101", int8(-101), common.ValueTypeInt8, "", "text/plain", false},
+		{"Test A Int8 error", "-101.98", nil, common.ValueTypeInt8, "", "text/plain", true},
+		{"Test A Int16+", "2001", int16(2001), common.ValueTypeInt16, "", "text/plain", false},
+		{"Test A Int16-", "-2001", int16(-2001), common.ValueTypeInt16, "", "text/plain", false},
+		{"Test A Int16 error", "-FF", nil, common.ValueTypeInt16, "", "text/plain", true},
+		{"Test A Int32+", "32000", int32(32000), common.ValueTypeInt32, "", "text/plain", false},
+		{"Test A Int32-", "-32000", int32(-32000), common.ValueTypeInt32, "", "text/plain", false},
+		{"Test A Int32 error", "-32.456", nil, common.ValueTypeInt32, "", "text/plain", true},
+		{"Test A Int64+", "214748364800", int64(214748364800), common.ValueTypeInt64, "", "text/plain", false},
+		{"Test A Int64-", "-214748364800", int64(-214748364800), common.ValueTypeInt64, "", "text/plain", false},
+		{"Test A Int64 error", "-21474.99", nil, common.ValueTypeInt64, "", "text/plain", true},
+		{"Test A Object", []byte(objectJson), expectedObject, common.ValueTypeObject, "", "application/json", false},
+		{"Test A Object error", "---", nil, common.ValueTypeObject, "", "application/json", true},
+		{"Test A Object error", "....", nil, common.ValueTypeObject, "", "text/plain", true},
+	}
+
+	for _, currentTest := range tests {
+		t.Run(currentTest.Name, func(t *testing.T) {
+			resource := model.DeviceResource{
+				Name: "test",
+				Properties: model.ResourceProperties{
+					MediaType: currentTest.MimeType,
+				},
+			}
+			cmdVal, err := handler.newCommandValue(resource, currentTest.Value, currentTest.Type, currentTest.ContentType)
+			if currentTest.ErrorExpected {
+				assert.Error(t, err, "Expected an Error")
+			} else {
+				require.NoError(t, err, "Unexpected an Error")
+				assert.Equal(t, cmdVal.Value, currentTest.Expected)
+			}
+		})
+	}
+}
