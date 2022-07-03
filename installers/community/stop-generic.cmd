6b79ec70cf49a5f6723beea15cd26b4b1b4b0544
magallardo <gallardo@tibco.com>
2022-07-03 12:14:33 -0400

    Removed unused scripts

--- installers/community/stop-generic.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-if "%1%"==""  set ARCH=""
-if "%1%"=="amd64"  set ARCH=""
-if "%1%"=="arm64"  set ARCH="-arm64"
-
-docker-compose -p edgex -f docker-compose-air-no-secty.yml -f docker-compose-air-no-secty-demo-generic.yml -f docker-compose-edgex-no-secty.yml down -v
